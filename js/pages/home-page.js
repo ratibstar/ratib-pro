5d45ef42444e0933aca1743ba58cd3fcad97b1ed
ratibstar <ratibstar@users.noreply.github.com>
2026-05-08 17:49:48 +0300

    update-20260508-174947

--- js/pages/home-page.js
+++ js/pages/home-page.js
@@ -287,11 +287,7 @@
         var gatewayRateStr = usdToGateway.toFixed(2);
         var displayApprox = (total * usdToDisplay).toFixed(2);
         var displayRateStr = usdToDisplay.toFixed(2);
-        if (displayCur !== gatewayCur) {
-            ngeniusNote = '<p class="small mb-0 mt-2 ratib-ngenius-currency-note">Card checkout is charged in <strong>' + gatewayCur + '</strong>. Approximate in ' + displayCur + ': <strong>' + displayCur + ' ' + displayApprox + '</strong> <span class="ratib-ngenius-rate-note">(USD × ' + displayRateStr + ')</span>.</p>';
-        } else {
-            ngeniusNote = '<p class="small mb-0 mt-2 ratib-ngenius-currency-note">Card checkout is charged in <strong>' + gatewayCur + '</strong>. Approximate total: <strong class="ratib-ngenius-sar-total">' + gatewayCur + ' ' + gatewayApprox + '</strong> <span class="ratib-ngenius-rate-note">(USD × ' + gatewayRateStr + ')</span>.</p>';
-        }
+        ngeniusNote = '<p class="small mb-0 mt-2 ratib-ngenius-currency-note">Card checkout is charged in <strong>' + displayCur + '</strong>: <strong>' + displayCur + ' ' + displayApprox + '</strong> <span class="ratib-ngenius-rate-note">(USD × ' + displayRateStr + ')</span>.</p>';
 
         console.log('updatePaymentSummary - subtotal:', subtotal, 'years:', years, 'planLabel:', planLabel, 'inputPlan.value:', inputPlanEl ? inputPlanEl.value : '');
 
