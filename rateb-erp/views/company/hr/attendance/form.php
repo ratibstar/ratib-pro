1bedffe2dba830751febed4c26e3d3200bd9f50b
ratibstar <ratibstar@users.noreply.github.com>
2026-06-18 22:49:49 +0300

    update-20260618-224949

--- rateb-erp/views/company/hr/attendance/form.php
+++ rateb-erp/views/company/hr/attendance/form.php
@@ -1,3 +1,16 @@
 <?php Rateb\App\Core\View::partial('hr-nav', ['hrActive' => 'attendance-daily']); ?>
+<?php
+$employeeOptions = $lookups['employees'] ?? [];
+if ($employeeOptions === []) {
+    ?>
+<div class="alert alert-warning mb-3">
+    <?php echo __('hr_attendance_need_employee'); ?>
+    <a class="alert-link ms-1" href="<?php echo rateb_url_with_ops_company(rateb_app_route('hr/employees/create')); ?>">
+        <?php echo __('create'); ?> <?php echo __('hr_employees'); ?>
+    </a>
+</div>
+    <?php
+}
+?>
 <?php Rateb\App\Core\View::partial('crud-form', get_defined_vars()); ?>
 <?php Rateb\App\Core\View::partial('hr-nav-end'); ?>
